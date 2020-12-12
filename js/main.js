adf0191737f8de3b5cb05bbdaadb533bbc516fd7
Amr Mohamed <63131532+amr-mohamed1@users.noreply.github.com>
2020-12-13 00:11:14 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$("document").ready(function () {
+
+    $('.count').countUp({
+        delay: 10,
+        time: 2500
+    });
+        $('.owl-carousel').owlCarousel({
+            rtl:true,
+            loop:true,
+            margin:30,
+            nav:true,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:3
+                }
+            }
+        });
+
+})
---
+++ js/mortgage.js
@@ -0,0 +1,6 @@
+$("document").ready(function () {
+    $(".btn_scroll").click(function() {
+        window.scrollTo({ top: 500, behavior: 'smooth'});
+      });
+
+})
