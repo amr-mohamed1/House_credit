154d9d28d35bb926b8d3d14d78edb52377f02ed6
Amr Mohamed <63131532+amr-mohamed1@users.noreply.github.com>
2020-12-19 18:29:50 +0200

    Add files via upload

--- js/mortgage.js
+++ js/mortgage.js
@@ -1,6 +1,6 @@
 $("document").ready(function () {
     $(".btn_scroll").click(function() {
-        window.scrollTo({ top: 520, behavior: 'smooth'});
+        window.scrollTo({ top: 590, behavior: 'smooth'});
       });
 
 $('.message .close')
